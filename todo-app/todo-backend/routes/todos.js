bb9d331d4ebc4e043bec6f6949e74a1c69014de2
clintam123 <nguyenhanh1074@gmail.com>
2022-01-12 12:22:45 +0700

    Exercise 12.10

--- todo-app/todo-backend/routes/todos.js
+++ todo-app/todo-backend/routes/todos.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const { Todo } = require("../mongo");
 const router = express.Router();
+const { getAsync, setAsync } = require("../redis");
+
+const todoCounter = async () => {
+  const count = await getAsync("count");
+  console.log(count);
+  return count ? setAsync("count", parseInt(count) + 1) : setAsync("count", 1);
+};
 
 /* GET todos listing. */
 router.get("/", async (_, res) => {
@@ -8,8 +15,18 @@ router.get("/", async (_, res) => {
   res.send(todos);
 });
 
+router.get("/statistics", async (_, res) => {
+  const count = await getAsync("count");
+
+  console.log("stats", count);
+
+  return res.json({ added_todos: count || 0 });
+});
+
 /* POST todo to listing. */
 router.post("/", async (req, res) => {
+  todoCounter();
+
   const todo = await Todo.create({
     text: req.body.text,
     done: false,
